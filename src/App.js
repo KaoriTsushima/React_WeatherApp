b3832115ca69e7be47532c4a879b49957b82430a
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-08-15 20:00:25 +0100

    changed all files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import "./styles.css";
-import Search from "./Search";
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Search />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
