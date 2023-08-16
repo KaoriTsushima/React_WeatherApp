7d16ab592e33f77f139d7fefd47a090da51943d3
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-08-16 11:07:33 +0100

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,17 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Search />
+      <footer>
+        <em>
+          <a
+            href="https://github.com/KaoriTsushima/React_WeatherApp"
+            target="_blank"
+          >
+            coded
+          </a>{" "}
+          by Kaori Tsushima
+        </em>
+      </footer>
     </div>
   );
 }
