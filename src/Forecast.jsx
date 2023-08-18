a9afb005f1686ad001433977f6003bcd647ee184
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-08-18 16:35:57 +0100

    added functioning forecast

--- src/Forecast.jsx
+++ src/Forecast.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Forecast.css";
+import ForecastDay from "./ForecastDay";
 import axios from "axios";
-import { WiCloud, WiDaySunny, WiRain } from "weather-icons-react";
 
 export default function Forecast(props) {
   console.log(props);
@@ -15,7 +15,7 @@ export default function Forecast(props) {
 
   function handleResponse(response) {
     console.log(response);
-    setForecast(response.daily);
+    setForecast(response.data.daily.slice(0, 5));
   }
 
   function load() {
@@ -27,42 +27,13 @@ export default function Forecast(props) {
     axios.get(apiUrl).then(handleResponse);
   }
   if (forecast) {
-    let forecasts = [
-      {
-        day: "Day1",
-        icon: <WiDaySunny size={50} color="orange" />,
-        temp: "High:20℃ Low: 10℃",
-      },
-      {
-        day: "Day2",
-        icon: <WiCloud size={50} color="gray" />,
-        temp: "High:20℃ Low: 10℃",
-      },
-      {
-        day: "Day3",
-        icon: <WiRain size={50} color="blue" />,
-        temp: "High:20℃ Low: 10℃",
-      },
-      {
-        day: "Day4",
-        icon: <WiRain size={50} color="blue" />,
-        temp: "High:20℃ Low: 10℃",
-      },
-      {
-        day: "Day5",
-        icon: <WiRain size={50} color="blue" />,
-        temp: "High:20℃ Low: 10℃",
-      },
-    ];
     return (
       <div className="Forecast">
         <div className="grid-forecast-container">
-          {forecasts.map(function (forecast, index) {
+          {forecast.map(function (dailyForecast, index) {
             return (
               <div key={index}>
-                <h3>{forecast.day}</h3>
-                <p>{forecast.icon}</p>
-                <p> {forecast.temp}</p>
+                <ForecastDay dailyForecast={dailyForecast} />
               </div>
             );
           })}
---
+++ src/ForecastDay.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+export default function ForecastDay(props) {
+  const maxTemp = `${Math.round(props.dailyForecast.temp.max)} ℃`;
+  const minTemp = `${Math.round(props.dailyForecast.temp.min)} ℃`;
+  const icon = props.dailyForecast.weather[0].icon;
+
+  function day() {
+    let date = new Date(props.dailyForecast.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
+  return (
+    <div>
+      <div className="Forecast-day">{day()}</div>
+      <img
+        alt="weather icon"
+        height={100}
+        src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+      />
+      <div className="Forecast-temperatures">
+        <span className="Forecast-temperature-max">{maxTemp}</span>
+        <span className="Forecast-temperature-min">{minTemp}</span>
+      </div>
+    </div>
+  );
+}
